8a1fb5176dde6f0ec4cf22f9696609e7137f0ec4
Dimitrios Milios <dimitrios.milios@gmail.com>
2009-06-05 21:17:55 +0100

    first commit

---
+++ src/stochastic.h
@@ -0,0 +1,15 @@
+/*
+ * stochastic.h
+ *
+ *  Created on: 05-Jun-2009
+ *      Author: Dimitrios Milios
+ *
+ *  Includes all the definitions of "stochastic" library
+ *  (only this one has to be included, so as to use the library)
+ */
+
+#ifndef STOCHASTIC_H_
+#define STOCHASTIC_H_
+
+
+#endif /* STOCHASTIC_H_ */
---
+++ test_src/main.cpp
@@ -0,0 +1,45 @@
+// =============================================================
+
+/* 'stochastic' is a library
+ * for performing operations between random variables
+ *
+ * main() is used for test purposes
+ *
+ */
+
+// =============================================================
+
+
+// load "stochastic" library
+#include "../src/stochastic.h"
+
+#include <stdio.h>
+
+
+int printArguments(int argc, char *argv[])
+{
+    int i;
+    printf("\nThe arguments were:\n");
+    for (i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *gnuplot;
+    gnuplot = popen("gnuplot", "w");
+
+    int x = 1;
+    fprintf(gnuplot, "plot %d\n", x);
+    fflush(gnuplot);
+
+    printf("Press enter to continue...");
+    fflush(stdout);
+    getchar();
+
+    fputs("exit \n", gnuplot);
+    pclose(gnuplot);
+
+    return printArguments(argc, argv);
+}
